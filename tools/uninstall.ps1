aa9a311f9d41b232d843200a1bdd1ed7b562441f
AraHaan <seandhunt_7@yahoo.com>
2022-04-10 00:41:22 -0400

    Place generator files in analyzers\dotnet\cs in package and update tools.
    Signed-off-by: AraHaan <seandhunt_7@yahoo.com>
    
    Fixes #12.

--- tools/uninstall.ps1
+++ tools/uninstall.ps1
@@ -1,13 +1,19 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-$analyzersPaths = Join-Path (Join-Path (Split-Path -Path $toolsPath -Parent) "analyzers") * -Resolve
+$analyzersDir = Join-Path (Split-Path -Path $toolsPath -Parent) "analyzers"
+if (-Not (Test-Path $analyzersDir))
+{
+    return
+}
+
+$analyzersPaths = Join-Path ( $analyzersDir ) * -Resolve
 
 foreach($analyzersPath in $analyzersPaths)
 {
     # Uninstall the language agnostic analyzers.
     if (Test-Path $analyzersPath)
     {
-        foreach ($analyzerFilePath in Get-ChildItem -Path "$analyzersPath\*.dll" -Exclude *.resources.dll)
+        foreach ($analyzerFilePath in Get-ChildItem $analyzersPath -Filter *.dll)
         {
             if($project.Object.AnalyzerReferences)
             {
@@ -38,7 +44,7 @@ foreach($analyzersPath in $analyzersPaths)
     $languageAnalyzersPath = join-path $analyzersPath $languageFolder
     if (Test-Path $languageAnalyzersPath)
     {
-        foreach ($analyzerFilePath in Get-ChildItem -Path "$languageAnalyzersPath\*.dll" -Exclude *.resources.dll)
+        foreach ($analyzerFilePath in Get-ChildItem $languageAnalyzersPath -Filter *.dll)
         {
             if($project.Object.AnalyzerReferences)
             {
